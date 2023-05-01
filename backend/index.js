11e661e9962cbd8c49f6446546b7eb2c1e325133
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-05-01 23:59:26 +0700

    Penambahan index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import session from 'express-session';
+// import session from 'express-session';
 
 const app = express();
 
-app.listen();
+app.listen(process.env.APP_PORT, () => {
+    console.log('Server up and running...');
+});
