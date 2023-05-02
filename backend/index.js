a02889cb7feaa1b5b609fdb087eaaec3976415d2
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-05-02 07:26:43 +0700

    Penambahan UserModel dan ProductModel

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,29 @@
 import express from 'express';
 import cors from 'cors';
-// import session from 'express-session';
+import session from 'express-session';
+import dotenv from 'dotenv';
 
 const app = express();
+
+dotenv.config();
+
+// Middleware
+app.use(session({
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: 'auto'
+    }
+}));
+
+
+app.use(cors ({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
+
+app.use(express.json());
 
 app.listen(process.env.APP_PORT, () => {
     console.log('Server up and running...');
