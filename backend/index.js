80929a8a2b386f306dcfc215c54a3e2606ad2533
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-05-03 13:31:28 +0700

    Penambahan Sessions

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import session from 'express-session';
 import dotenv from 'dotenv';
-// import db from './config/Database.js';
+import db from './config/Database.js';
 import UserRoute from './routes/UserRoute.js';
 import ProductRoute from './routes/ProductRoute.js';
 import AuthRoute from './routes/AuthRoute.js';
+import SequelizeStore from 'connect-session-sequelize';
 
 const app = express();
+
+const sessionStore = SequelizeStore(session.Store);
+const store = new sessionStore({
+    db: db
+});
 
 /* (async() => {
     await db.sync();
@@ -20,6 +26,7 @@ app.use(session({
     secret: process.env.SESS_SECRET,
     resave: false,
     saveUninitialized: true,
+    store: store,
     cookie: {
         secure: 'auto'
     }
@@ -36,6 +43,8 @@ app.use(express.json());
 app.use(UserRoute);
 app.use(ProductRoute);
 app.use(AuthRoute);
+
+// store.sync();
 
 app.listen(process.env.APP_PORT, () => {
     console.log('Server up and running...');
