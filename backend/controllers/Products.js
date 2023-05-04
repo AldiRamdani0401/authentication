15f4210b8ee8976a61ba97aa62319ebb992e5448
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-05-04 11:40:31 +0700

    Penambahan getProducts

--- backend/controllers/Products.js
+++ backend/controllers/Products.js
@@ -1,7 +1,29 @@
 import Product from "../models/ProductModel.js";
+import User from "../models/UserModel.js";
 
-export const getProducts = (req, res) => {
-
+export const getProducts = async(req, res) => {
+    try {
+        let response;
+        if(req.role === "admin"){
+            response = await Product.findAll({
+                include:[{
+                    model: User
+                }]
+            });
+        }else{
+            response = await Product.findAll({
+                where:{
+                    userId: req.userId
+                },
+                include:[{
+                    model: User
+                }]
+            });
+        }
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 }
 
 export const getProductById = (req, res) => {
