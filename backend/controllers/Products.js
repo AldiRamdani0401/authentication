3237085275ccd98b4ec104b9f019def3bb877636
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-05-05 11:02:57 +0700

    Penambahan createProduct

--- backend/controllers/Products.js
+++ backend/controllers/Products.js
@@ -6,17 +6,21 @@ export const getProducts = async(req, res) => {
         let response;
         if(req.role === "admin"){
             response = await Product.findAll({
+                attributes: ['uuid', 'name', 'price'],
                 include:[{
-                    model: User
+                    model: User,
+                    attributes: ['name', 'email']
                 }]
             });
         }else{
             response = await Product.findAll({
+                attributes: ['uuid', 'name', 'price'],
                 where:{
                     userId: req.userId
                 },
                 include:[{
-                    model: User
+                    model: User,
+                    attributes: ['name', 'email']
                 }]
             });
         }
@@ -30,8 +34,18 @@ export const getProductById = (req, res) => {
 
 }
 
-export const createProduct = (req, res) => {
-
+export const createProduct = async(req, res) => {
+    const {name, price} = req.body;
+    try {
+        await Product.create({
+            name: name,
+            price: price,
+            userId: req.userId
+        });
+        res.status(201).json({msg: "Product Created Successfuly"});
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 }
 
 export const updateProduct = (req, res) => {
