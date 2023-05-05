1c549b0dbeb8f81b8d3fec26bf017c88fec4b816
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-05-05 18:52:27 +0700

    Penambahan updateProduct

--- backend/controllers/Products.js
+++ backend/controllers/Products.js
@@ -83,8 +83,33 @@ export const createProduct = async(req, res) => {
     }
 }
 
-export const updateProduct = (req, res) => {
-
+export const updateProduct = async(req, res) => {
+    try {
+        const product = await Product.findOne({
+            where:{
+                uuid: req.params.id
+            }
+        });
+        if(!product) return res.status(404).json({msg: "Data tidak ditemukan"});
+        const {name, price} = req.body;
+        if(req.role === "admin"){
+            await Product.update({name, price},{
+                where: {
+                    id: product.id
+                }
+            });
+        }else{
+            if(req.userId !== product.userId) return res.status(403).json({msg: "Akses terlarang"});
+            await Product.update({name, price},{
+                where:{
+                    [Op.and]:[ {id: product.id} , {userId: req.userId}]
+                },
+            });
+        }
+        res.status(200).json({msg: "Product updated successfuly"});
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 }
 
 export const deleteProduct = (req, res) => {
