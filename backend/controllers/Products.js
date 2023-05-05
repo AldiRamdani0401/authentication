f7a6b180a5b344e1b6caba523a839e35c4968457
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-05-05 18:15:17 +0700

    Penambahan getProductById

--- backend/controllers/Products.js
+++ backend/controllers/Products.js
@@ -1,5 +1,6 @@
 import Product from "../models/ProductModel.js";
 import User from "../models/UserModel.js";
+import {Op} from "sequelize";
 
 export const getProducts = async(req, res) => {
     try {
@@ -30,8 +31,42 @@ export const getProducts = async(req, res) => {
     }
 }
 
-export const getProductById = (req, res) => {
-
+export const getProductById = async(req, res) => {
+    try {
+        const product = await Product.findOne({
+            where:{
+                uuid: req.params.id
+            }
+        });
+        if(!product) return res.status(404).json({msg: "Data tidak ditemukan"});
+        let response;
+        if(req.role === "admin"){
+            response = await Product.findOne({
+                attributes: ['uuid', 'name', 'price'],
+                where:{
+                    id: product.id
+                },
+                include:[{
+                    model: User,
+                    attributes: ['name', 'email']
+                }]
+            });
+        }else{
+            response = await Product.findOne({
+                attributes: ['uuid', 'name', 'price'],
+                where:{
+                    [Op.and]:[ {id: product.id} , {userId: req.userId}]
+                },
+                include:[{
+                    model: User,
+                    attributes: ['name', 'email']
+                }]
+            });
+        }
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 }
 
 export const createProduct = async(req, res) => {
