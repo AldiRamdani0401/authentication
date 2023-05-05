84452e3753b36a4e3202bc58707783e3d20d6114
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-05-05 22:59:22 +0700

    Penambahan Component Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Login/>}/>
           <Route path="/dashboard" element={<Dashboard/>}/>
         </Routes>
       </BrowserRouter>
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,38 +1,35 @@
 import React from 'react'
 
 const Login = () => {
-  return (
+    return (
     <section className="hero has-background-grey-light is-fullheight is-fullwidth">
-      <div className="hero-body">
-        <div className="container">
-            <div className="columns is-centered">
-                <div className="column is-4">
-                    <form className='box'>
-                        <div className="field">
-                            <label className="label">Email</label>
-                                <div className="control">
-                                    <input type="text" className="input" placeholder='Email'/>
-                                </div>
-                            <div className="field">
-                                <label className="label">Password</label>
-                                <div className="control">
-                                    <input type="password" className="input" placeholder='*****'/>
-                                </div>
-                            </div>
+        <div className="hero-body">
+            <div className="container">
+                <div className="columns is-centered">
+                    <div className="column is-4">
+                        <form className='box'>
                             <div className="field">
                                 <label className="label">Email</label>
-                                <div className="control">
-                                    <input type="text" className="input" placeholder='Email'/>
+                                    <div className="control">
+                                        <input type="text" className="input" placeholder='Email'/>
+                                    </div>
+                                <div className="field">
+                                    <label className="label">Password</label>
+                                    <div className="control">
+                                        <input type="password" className="input" placeholder='*****'/>
+                                    </div>
+                                </div>
+                                <div className="field">
+                                    <button className="button is-success is-fullwidth">Login</button>
                                 </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-      </div>
     </section>
-  )
+    )
 }
 
 export default Login
