404fccea4a7a69cb60e0780af7245df1bf920f2a
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-05-07 11:25:37 +0700

    Penerapan Proteksi untuk halaman Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom'
 import { getMe } from '../features/authSlice'
 
 const Dashboard = () => {
+  
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { isError } = useSelector((state => state.auth));
