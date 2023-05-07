5d899a1e24da09586788e473494fb1237a282d89
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-05-07 10:36:03 +0700

    Proteksi pada authSlice.js untuk dashboard

--- frontend/src/features/authSlice.js
+++ frontend/src/features/authSlice.js
@@ -24,6 +24,22 @@ export const LoginUser = createAsyncThunk("user/LoginUser", async(user, thunkAPI
     }
 });
 
+export const getMe = createAsyncThunk("user/getMe", async(_, thunkAPI) => {
+    try {
+        const response = await axios.get('http://localhost:5000/me');
+        return response.data;
+    } catch (error) {
+        if(error.response){
+            const message = error.response.data.msg;
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+});
+
+export const LogOut = createAsyncThunk("user/LogOut", async() => {
+    await axios.delete('http://localhost:5000/logout');
+});
+
 export const authSlice = createSlice({
     name: "auth",
     initialState,
@@ -40,6 +56,21 @@ export const authSlice = createSlice({
             state.user = action.payload;
         });
         builder.addCase(LoginUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.message = action.payload;
+        });
+
+        // Get User Login
+        builder.addCase(getMe.pending, (state) =>{
+            state.isLoading = true;
+        });
+        builder.addCase(getMe.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isSuccess = true;
+            state.user = action.payload;
+        });
+        builder.addCase(getMe.rejected, (state, action) => {
             state.isLoading = false;
             state.isError = true;
             state.message = action.payload;
