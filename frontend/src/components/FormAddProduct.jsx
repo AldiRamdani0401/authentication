b8c20b7b2267334e5a21f855487cf1aa0eb6edd4
AldiRamdani0401 <ramdanialdi889@gmail.com>
2023-05-07 08:18:15 +0700

    Penambahan FormAddProduct

---
+++ frontend/src/components/FormAddProduct.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const FormAddProduct = () => {
+    return (
+        <div>
+            <h1 className='title'>Products</h1>
+            <h2 className='subtitle'>Add New Product</h2>
+            <div className="card is-shadowless">
+                <div className="card-content">
+                    <div className="content">
+                        <form>
+                            <div className="field">
+                            <label className="label">Product Name</label>
+                                <div className="control">
+                                    <input type="text" className="input" placeholder='Product Name'/>
+                                </div>
+                            </div>
+                            <div className="field">
+                            <label className="label">Price</label>
+                                <div className="control">
+                                    <input type="text" className="input" placeholder='Price'/>
+                                </div>
+                            </div>
+                            <div className="field">
+                                <div className="control">
+                                    <button className="button is-success">Save</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FormAddProduct
